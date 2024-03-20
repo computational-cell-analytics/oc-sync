e78d391333c2cae0024cc4ec0065ab980e21d3db
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-03-20 16:02:01 +0100

    Fix issues in sync

--- oc_sync/oc_sync.py
+++ oc_sync/oc_sync.py
@@ -1,8 +1,9 @@
 import os
+import time
 from owncloud import HTTPResponseError
 
 
-def _sync_remote_folder(client, remote_src, local_dst):
+def _sync_remote_folder(client, remote_src, local_dst, verbose):
     files = client.list(remote_src)
     for fi in files:
         name = fi.path
@@ -11,17 +12,18 @@ def _sync_remote_folder(client, remote_src, local_dst):
         if fi.file_type == "file":
             if os.path.exists(dst):
                 continue
-            print("Syncing file", src, "to", dst)
+            if verbose:
+                print("Syncing file", src, "to", dst)
             os.makedirs(os.path.split(dst)[0], exist_ok=True)
             client.get_file(src, dst)
         elif fi.file_type == "dir":
-            _sync_remote_folder(client, src, local_dst)
+            _sync_remote_folder(client, src, local_dst, verbose)
         else:
             raise RuntimeError(f"Unknown file type {fi.file_type}")
 
 
-def sync_from_owncloud(client, remote_src, local_dst):
-    _sync_remote_folder(client, remote_src, local_dst)
+def sync_from_owncloud(client, remote_src, local_dst, verbose=True):
+    _sync_remote_folder(client, remote_src, local_dst, verbose)
 
 
 def oc_file_exists(client, path):
@@ -44,22 +46,24 @@ def oc_makedirs(client, path):
         raise e
 
 
-def _sync_local_folder(client, local_src, remote_dst):
+def _sync_local_folder(client, local_src, remote_dst, verbose):
     files = os.listdir(local_src)
     for name in files:
         src = os.path.join(local_src, name)
         dst = os.path.join(remote_dst, name)
-        print("Sync", src, dst)
 
         if os.path.isfile(src):
             if oc_file_exists(client, dst):
                 continue
+            if verbose:
+                print("Syncing file", src, "to", dst)
             oc_makedirs(client, os.path.split(dst)[0])
             client.put_file(dst, src)
+            time.sleep(1)
 
         elif os.path.isdir(src):
-            _sync_local_folder(client, src, os.path.join(remote_dst, name))
+            _sync_local_folder(client, src, os.path.join(remote_dst, name), verbose)
 
 
-def sync_to_owncloud(client, local_src, remote_dst):
-    _sync_local_folder(client, local_src, remote_dst)
+def sync_to_owncloud(client, local_src, remote_dst, verbose=True):
+    _sync_local_folder(client, local_src, remote_dst, verbose)
